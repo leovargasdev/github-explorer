7de36828803fde60cbaee640175fd92dc25ac85c
Leo Vargas <leu1607@hotmail.com>
2020-05-14 15:08:08 -0300

    Estilizando e adicionando conteúdo ao component Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {FiChevronRight} from 'react-icons/fi'
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo-github-explorer.svg';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Repositórios Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório"/>
+        <button type="button">Pesquisar Repositório</button>
+
+      </Form>
+      <Repositories>
+        <a href="/repository">
+          <img src="https://avatars3.githubusercontent.com/u/11177716?s=460&u=c9e54ca2ea76850493ae4b9c34e029ec2e613199&v=4" alt="Image Profile"/>
+          <div>
+            <strong>Trabalho-Prog-I-Toon-World</strong>
+            <span>
+              Neste repositorio estará contido o trabalho de Prog I, um interpretador da
+              linguagem Toon World, baseada em Java. Para saber sobre ele, veja a documentaçao da linguagem.
+            </span>
+          </div>
+          <FiChevronRight size={40}/>
+        </a>
+      </Repositories>
+    </>
+  );
 }
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import {shade} from 'polished';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  margin-top: 80px;
+`;
+
+export const Form = styled.div`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    color: #3a3a3a;
+    border-radius: 5px 0 0 5px;
+
+    &::placeholder{
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #FFF;
+    font-weight: bold;
+    transition: background-color 0.3s;
+
+    &:hover{
+      background: ${shade(0.1, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a{
+    display: flex;
+    align-items: center;
+    background: #FFF;
+    width: 100%;
+    padding: 24px 20px;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    &:hover{
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-bottom: 10px;
+    }
+  }
+
+  img{
+    width: 70px;
+    height: 70px;
+    border-radius: 50px;
+  }
+
+  div {
+    margin-left: 16px;
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+
+    span {
+      font-size: 16px;
+      font-weight: 300;
+      color: #a8a8b3;
+    }
+  }
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
